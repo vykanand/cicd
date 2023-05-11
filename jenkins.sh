a0f1a3cac33f8458668eb5590cc7a0d35da60c6a
vykanand <vykanand@gmail.com>
2023-05-12 01:06:24 +0530

    jenkins update

--- jenkins.sh
+++ jenkins.sh
@@ -35,8 +35,8 @@ sudo ln -sf /usr/local/share/phantomjs/bin/phantomjs /usr/local/bin
 
 # Install Jenkins
 ## Before install is necessary to add Jenkins to trusted keys and source list
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
 
